924442f2e3b09b1ab06039c8599779ac8435ba7f
Han <seungyoonee@gmail.com>
2022-04-22 21:38:07 -0400

    4/22

---
+++ Java/Max_Sum_Subarray_ver1.java
@@ -0,0 +1,19 @@
+public class MaxSumSubarray {
+	public static int findMaxSumSubarray(int[] arr, int k) {
+		int maxValue = Integer.MIN_VALUE;
+		int currentRunningSum = 0;
+
+		for (int i = 0; i < arr.length; i++) {
+			currentRunningSum += arr[i];
+			if (i >= k - 1) {
+				maxValue = Math.max(maxValue, currentRunningSum);
+				currentRunningSum -= arr[i - (k - 1)];
+			}
+		}
+		return maxValue;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(findMaxSumSubarray(new int[] {4, 2, 1, 6, 3, 7, 9, 3, 6}, 3));
+	}
+}
