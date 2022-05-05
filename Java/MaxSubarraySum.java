f7e86625b67eaf572b47693c0f1ff6e52993c318
Han <seungyoonee@gmail.com>
2022-05-04 23:16:32 -0400

    5/4

---
+++ Java/MaxSubarraySum.java
@@ -0,0 +1,25 @@
+public class MaxSubarraySum {
+    
+    public static int getMaxSum(int[] nums) {
+
+        int maxSum = nums[0];
+        int curSum = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            curSum = Math.max(nums[i] + curSum, nums[i]);
+            maxSum = Math.max(curSum, maxSum);
+        }
+
+        return maxSum;
+    }
+
+    public static void main(String args[]) {
+        int[] test1 = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        int[] test2 = {1};
+        int[] test3 = {5, 4, -1, 7, 8};
+
+        System.out.println(getMaxSum(test1)); // 6
+        System.out.println(getMaxSum(test2)); // 1
+        System.out.println(getMaxSum(test3)); // 23
+    }
+}
