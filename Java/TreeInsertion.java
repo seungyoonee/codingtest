46eb5e35a10c7e2b723ff80dbed1f5902ae4a68d
Han <seungyoonee@gmail.com>
2022-05-16 17:56:43 -0400

    5/16

---
+++ Java/TreeInsertion.java
@@ -0,0 +1,12 @@
+public class TreeInsertion {
+    public class Node<T> {
+        T data;
+        Node left, right;
+
+        Node(T data) {
+            this.data = data;
+            left = null;
+            right = null;
+        }
+    }
+}
